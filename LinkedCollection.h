468e821ea845099e81175bf35bcc59e78578bbdc
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-12 23:51:28 +0300

    add ctwo construtors  defualt and copy

--- LinkedCollection.h
+++ LinkedCollection.h
@@ -5,12 +5,23 @@
 #include <optional> // For using std::optional
 #include <iostream>
 
-
+/*
+ The LinkedCollection class implements a doubly linked list.
+ Each node in the list stores a value and pointers to the next and previous nodes.
+ */
 class LinkedCollection {
 private:
     Node* head; // Pointer to the first node in the list
     Node* tail; // Pointer to the last node in the list
-   
+public:
+    // Default constructor (creates an empty list)
+    LinkedCollection(void); 
+    // Initializes head and tail as nullptr
+
+    // Copy constructor
+    LinkedCollection(const LinkedCollection& other); 
+    // Copies elements from another list
+
 };
 
-#endif 
+#endif // LINKEDCOLLECTION_H
