52da541c76d1826310bf437c93f6e4f40943da89
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-12 23:48:54 +0300

    add new file with two node pointers to the firt node and to the last node in the list

---
+++ LinkedCollection.h
@@ -0,0 +1,16 @@
+#ifndef LINKEDCOLLECTION_H
+#define LINKEDCOLLECTION_H
+
+#include "NodeStruct.h"
+#include <optional> // For using std::optional
+#include <iostream>
+
+
+class LinkedCollection {
+private:
+    Node* head; // Pointer to the first node in the list
+    Node* tail; // Pointer to the last node in the list
+   
+};
+
+#endif 
