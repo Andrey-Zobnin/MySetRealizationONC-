5d49fb836b3419138674a6616c6d41e05d690ef8
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-13 18:52:36 +0300

    add functionality with zero

--- LinkedCollection.cpp
+++ LinkedCollection.cpp
@@ -4,40 +4,53 @@
 #include <optional>
 
 // Конструктор по умолчанию
-LinkedCollection::LinkedCollection();
+LinkedCollection::LinkedCollection()
+{}
 
 // Конструктор копирования
-LinkedCollection::LinkedCollection(const LinkedCollection& other);
+LinkedCollection::LinkedCollection(const LinkedCollection& other)
+{}
 
 // Деструктор
-LinkedCollection::~LinkedCollection();
+LinkedCollection::~LinkedCollection()
+{}
 
 // Метод для добавления элемента
-void LinkedCollection::AddElement(int value);
+void LinkedCollection::AddElement(int value)
+{}
 
 // Метод для удаления элемента
-bool LinkedCollection::RemoveElement(int value);
+bool LinkedCollection::RemoveElement(int value)
+{}
 
 // Метод для поиска максимального элемента
-std::optional<int> LinkedCollection::FindMax() const;
+std::optional<int> LinkedCollection::FindMax() const
+{}
 
 // Метод для поиска минимального элемента
-std::optional<int> LinkedCollection::FindMin() const;
+std::optional<int> LinkedCollection::FindMin() const
+{}
 
 // Метод для получения размера коллекции
-int LinkedCollection::GetSize() const;
+int LinkedCollection::GetSize() const
+{}
 
 // Метод для итерации по элементам
-void LinkedCollection::Iterate() const;
+void LinkedCollection::Iterate() const
+{}
 
 // Метод для получения адреса первого элемента
-Node* LinkedCollection::GetAddress() const;
+Node* LinkedCollection::GetAddress() const
+{}
 
 // Оператор сложения
-LinkedCollection LinkedCollection::operator+(const LinkedCollection& other) const;
+LinkedCollection LinkedCollection::operator+(const LinkedCollection& other) const
+{}
 
 // Оператор сравнения
-bool LinkedCollection::operator==(const LinkedCollection& other) const;
+bool LinkedCollection::operator==(const LinkedCollection& other) const
+{}
 
 // Метод для очистки коллекции
-void LinkedCollection::Clear();
+void LinkedCollection::Clear()
+{}
