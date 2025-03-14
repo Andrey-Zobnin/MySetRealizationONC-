afae100a6cd5ee65695c820494df3d545f7b517c
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-14 20:10:04 +0300

    update add constructor

--- LinkedCollection.cpp
+++ LinkedCollection.cpp
@@ -1,56 +1,19 @@
 #include "LinkedCollection.h"
-#include <algorithm>
 #include <iostream>
-#include <optional>
 
-// Конструктор по умолчанию
-LinkedCollection::LinkedCollection()
-{}
+// Default constructor
+LinkedCollection::LinkedCollection() : head(nullptr), tail(nullptr) {}
 
-// Конструктор копирования
-LinkedCollection::LinkedCollection(const LinkedCollection& other)
-{}
+// Copy constructor
+LinkedCollection::LinkedCollection(const LinkedCollection& other) : head(nullptr), tail(nullptr) {
+    // copy elements from other collection to head
 
-// Деструктор
-LinkedCollection::~LinkedCollection()
-{}
-
-// Метод для добавления элемента
-void LinkedCollection::AddElement(int value)
-{}
-
-// Метод для удаления элемента
-bool LinkedCollection::RemoveElement(int value)
-{}
-
-// Метод для поиска максимального элемента
-std::optional<int> LinkedCollection::FindMax() const
-{}
-
-// Метод для поиска минимального элемента
-std::optional<int> LinkedCollection::FindMin() const
-{}
-
-// Метод для получения размера коллекции
-int LinkedCollection::GetSize() const
-{}
-
-// Метод для итерации по элементам
-void LinkedCollection::Iterate() const
-{}
-
-// Метод для получения адреса первого элемента
-Node* LinkedCollection::GetAddress() const
-{}
-
-// Оператор сложения
-LinkedCollection LinkedCollection::operator+(const LinkedCollection& other) const
-{}
-
-// Оператор сравнения
-bool LinkedCollection::operator==(const LinkedCollection& other) const
-{}
-
-// Метод для очистки коллекции
-void LinkedCollection::Clear()
-{}
+    Node* current = other.head;
+    // current->PointerForPreviosNode = nullptr; Set previous pointer of the first node to nullptr
+    while (current != nullptr) {
+        // use addElement method
+        addElement(current->dataOfElement);
+        // Set previous pointer of the current node to the next node
+        current = current->PointerForNextNode;
+    }
+}
