1bbcf55f3f0e15f5137bea102c840370bacb028f
Andrey-Zobnin <andy.zobnin@gmail.com>
2025-03-15 11:28:32 +0300

    main.cpp to test.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
-#include "LinkedCollection.h"
-#include <iostream>
+#include "test.h"
 
 int main() {
-    // Создаем первый список
-    LinkedCollection* list1 = new LinkedCollection(); // Используем динамическую память
-    list1->AddElement(10);
-    list1->AddElement(20);
-    list1->AddElement(30);
-
-    // Создаем второй список
-    LinkedCollection* list2 = new LinkedCollection();
-    list2->AddElement(40);
-    list2->AddElement(50);
-
-    // Объединяем списки
-    LinkedCollection* list3 = new LinkedCollection(*list1 + *list2);
-    std::cout << "Объединенный список: ";
-    list3->Iterate();
-
-    // Уничтожаем объекты
-    list1->Destroy(); // Уничтожаем list1
-    list2->Destroy(); // Уничтожаем list2
-    list3->Destroy(); // Уничтожаем list3
+    testAddElement();
+    testRemoveElement();
+    testFindMax();
+    testFindMin();
+    testGetSize();
+    testConcatenation();
+    testEquality();
+    testCopyConstructor();
+    testGetAddress();
 
     return 0;
 }
